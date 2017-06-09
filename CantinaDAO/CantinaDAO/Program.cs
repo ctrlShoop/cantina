2a11ff7ff2d33a6b775cf587c0fa86677fd11b58
ctrlShoop <almeidalucasde28@gmail.com>
2017-06-09 20:50:48 -0300

    Parte do CantinaDAO
    incompleta

---
+++ CantinaDAO/CantinaDAO/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CantinaBD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Environment.GetEnvironmentVariable("HOMEPATH") + "\\Desktop\\";
+
+            SQLiteConnection con =
+                new SQLiteConnection("Data Source=" + path + "MyDatabase.sqlite;Version=3");
+            con.Open();
+
+            string sql = "CREATE TABLE T_PRODUTO "
+              + "("
+                + "CODIGO     INT PRIMARY KEY  NOT NULL,"
+                + " NOME        CHAR(50), "
+                + " PRECO            DOUBLE, "
+                + " QUANTIDADE      INT"
+              + ")";
+            SQLiteCommand command = new SQLiteCommand(sql, con);
+            command.ExecuteNonQuery();
+            con.Close();
+            Console.ReadKey();
+
+        }
+    }
+}
